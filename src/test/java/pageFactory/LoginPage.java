aaab9295dfe95f049881e29d9090af8456c47cbc
pavisen <pavithrasenthurai@gmail.com>
2024-10-23 14:37:28 -0400

    commit

---
+++ src/test/java/pageFactory/LoginPage.java
@@ -0,0 +1,46 @@
+package pageFactory;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import drivers.DriverFactory;
+import utilities.ConfigReader;
+
+public class LoginPage {
+    public  WebDriver driver = DriverFactory.getDriver();
+	String baseurl = ConfigReader.loginPage();
+	@FindBy(id="username")
+	WebElement username;
+	@FindBy(id="password")
+	WebElement password;
+	@FindBy(id="login")
+	WebElement loginbtn;
+    public LoginPage() {
+
+		PageFactory.initElements(driver, this);
+
+	}
+//To get LOg in URL
+	public void getbaseurl() {
+		driver.get(baseurl);
+	}
+	public void enterUsername(String userName) {
+
+		username.sendKeys(userName);
+
+	}
+
+	public void enterPassword(String passWord) {
+		password.sendKeys(passWord);
+	}
+
+	public void clickLogin() {
+		loginbtn.click();
+	}
+
+	
+	
+    
+}
