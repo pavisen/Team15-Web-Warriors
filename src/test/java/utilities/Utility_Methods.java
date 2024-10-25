00fe8dbd3c4bb8fae226776b0b6d5b07ee062e2a
[AnbuSDET] <arumuga.anbuselvi@gmail.com>
2024-10-24 20:42:45 -0400

    Login Page

--- src/test/java/utilities/Utility_Methods.java
+++ src/test/java/utilities/Utility_Methods.java
@@ -21,34 +21,12 @@ public class Utility_Methods {
     public  WebDriver driver = DriverFactory.getDriver();
     public  String ExcelPath = ConfigReader.getexcelfilepath();
     WebDriverWait webDriverWait = new WebDriverWait(driver, Duration.ofSeconds(20));
+    
     public  void waitForElement(WebElement element) {
 
         webDriverWait.until(ExpectedConditions.visibilityOf(element));
 
-    }
-
-    public void enterPythonCodeForPractice(String code, WebElement element)
-    {
-        new Actions(driver).keyDown(Keys.CONTROL).sendKeys("a").keyUp(Keys.CONTROL).sendKeys(Keys.DELETE).perform();
-        String[] str1 = code.split("\n");
-        for (int i = 0; i < str1.length; i++) {
-            if (str1[i].equalsIgnoreCase("\\b")) {
-                element.sendKeys(Keys.BACK_SPACE);
-            } else {
-                element.sendKeys(str1[i]);
-                element.sendKeys(Keys.ENTER);
-            }
-        }
-    }
-    public  boolean enterPythonCode(WebElement element, String code) {
-        LoggerLoad.info("Before sending keys to " + element.getText() );
-        try {
-            new Actions(driver).sendKeys(element, code).perform();
-        } catch(Exception e) {
-            element.sendKeys(code);
-        }
-        return true;
-    }
+    }       
 
     public String getResultfromExcel(String sheetname, int rownumber) throws InvalidFormatException, IOException {
         ExcelReader reader = new ExcelReader();
@@ -64,5 +42,6 @@ public class Utility_Methods {
         String code = testdata.get(rownumber).get("PythonCode");
         return code;
     }
-
+    
+   
 }
